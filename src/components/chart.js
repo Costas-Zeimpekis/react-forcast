dc8d412c01ea4d844aad969192c0e3cc8e44be9c
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-07-26 11:49:23 +0300

    Added Google Maps

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,21 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 
 export default (props) => {
   return (
     <div>
-      <Sparklines data={props.data} width={180} height={120} >
+      <Sparklines data={props.data} width={150} height={100} margin={5}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
     </div>
   )
+}
+
+
+function average(data) {
+  return _.round(_.sum(data) / data.length);
 }
